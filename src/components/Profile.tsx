94fd7bf33f17937558c06ef69f31ef3db07eca08
Dessamgl <dessa1413@gmail.com>
2021-02-25 23:54:09 -0300

    Day 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(challengesContext);
+
  return (
    <div className={styles.profileContainer}>
      <img src="https://github.com/Dessamgl" alt="Andressa"/>
@@ -8,7 +13,7 @@ export function Profile() {
        <strong>Andressa Gaspar</strong>
        <p>
         <img src="icons/level.svg" alt="Level"/>
-         Level 1
+         Level {level}
       </p>
      </div>
    </div>
